2b99c3ee731c5bc40cfc7897c4c44cf785f0c431
Chaim Shteinberg <a0533197133@gmail.com>
2025-06-25 15:38:41 +0300

    oop Rectangle

--- oop_basics.js
+++ oop_basics.js
@@ -3,7 +3,24 @@
 function Phone(brand, model) {
     this.brand = brand;
     this.model = model;
-    this.datails = function(){
+    this.details = function(){
         console.log(`Phone: ${this.brand} ${this.model}`);        
     }
 }
+
+const myPhone = new Phone("QLYX", "D9");
+myPhone.details();
+
+// Exercise 2 - Rectangle
+
+function Rectangle(width, height){
+    this.width = width;
+    this.height = height;
+    this.area = function(){
+        return width * height;
+    }
+}
+
+const myRectangle = new Rectangle(5, 4);
+console.log(myRectangle.area());
+
